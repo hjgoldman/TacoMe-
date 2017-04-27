677ef081265727b09159fada35ebaaa376630e01
hjgoldman <haydengoldman@gmail.com>
2017-04-27 14:57:34 -0500

    Added google api to watch

--- Watch Extension/InterfaceController.swift
+++ Watch Extension/InterfaceController.swift
@@ -40,16 +40,137 @@ class InterfaceController: WKInterfaceController, CLLocationManagerDelegate {
     }
     @IBAction func tacoButton() {
         
-        print("taco pressed!")
+        
+        DispatchQueue.global().async {
+            
+            self.getGoogleData()
+            sleep(2)
+            // switch to the main thread to run UI specific tasks
+            DispatchQueue.main.async {
+                
+                if self.locations.count == 0 {
+                    
+                    
 
-        let action1 = WKAlertAction(title: "🌮", style: .default) {
-        
-            print("ok")
+                    let cancelAction = WKAlertAction(title: "Dismiss", style: .default) {
+                        self.locations.removeAll()
+                    }
+                    
+                    self.presentAlert(withTitle: "No Taco Found", message: "☹️", preferredStyle: .actionSheet, actions: [cancelAction])
+
+
+                } else {
+                    
+                    
+                    let closestTaco = self.locations[0]
+                    
+                    guard let distance = closestTaco.distanceFromUser else {
+                        return
+                    }
+                    
+                    let distanceInMiles = String(format: "%.2f", distance / 1609.34)
+
+                    
+                    let action1 = WKAlertAction(title: "🌮", style: .default) {
+                        
+                        print("ok")
+                    }
+                    let cancelAction = WKAlertAction(title: "🚫", style: .cancel) {}
+                    
+                    self.presentAlert(withTitle: "Taco Found!", message: "Closest Taco: \n \(closestTaco.name!) \n \(distanceInMiles) miles away", preferredStyle: .actionSheet, actions: [action1,cancelAction])
+                    
+                }
+            }
         }
-        let cancelAction = WKAlertAction(title: "🚫", style: .cancel) {}
-        
-        presentAlert(withTitle: "Taco Found!", message: "", preferredStyle: .actionSheet, actions: [action1,cancelAction])
+
         
     }
+    
+    func getGoogleData() {
+        
+        guard let lat = self.locationManager.location?.coordinate.latitude else {
+            return
+        }
+        
+        guard let lng = self.locationManager.location?.coordinate.longitude else {
+            return
+        }
+        
+        let headers = [
+            "cache-control": "no-cache",
+            ]
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(lat)%2C\(lng)%0A&radius=2000&type=resturant&keyword=taco&pagetoken&key=AIzaSyBbvw_RKKdzBigdZGjXTJZjgC3IMJVV6rU")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            
+            if (error != nil) {
+                print(error!)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse!)
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+            
+            let results = json["results"] as! [[String:Any]]
+            
+            for item in results {
+                
+                let geometry = item["geometry"] as! [String:Any]
+                let location = geometry["location"] as! [String:Any]
+                
+                let name = item["name"] as? String
+                let locationLat = location["lat"] as? Double
+                let locationLng = location["lng"] as? Double
+                let place_id = item["place_id"] as? String
+                let price_level = item["price_level"] as? Int
+                let rating = item["rating"] as? Double
+                let vicinity = item["vicinity"] as? String
+                
+                let itemLocation = Location()
+                
+                if let opening_hours = item["opening_hours"] {
+                    let openingHours = opening_hours as! [String:Any]
+                    if let open_now = openingHours["open_now"] {
+                        itemLocation.open_now = open_now as? Bool
+                    }
+                }
+                
+                itemLocation.locationLat = locationLat
+                itemLocation.locationLng = locationLng
+                itemLocation.name = name
+                itemLocation.place_id = place_id
+                itemLocation.price_level = price_level
+                itemLocation.rating = rating
+                itemLocation.vicinity = vicinity
+                
+                self.locations.append(itemLocation)
+            }
+            
+            print(self.locations.count)
+            self.findClosestTaco()
+        })
+        dataTask.resume()
+    }
+    
+    //Finding the location that is closest to the user
+    func findClosestTaco() {
+        
+        var distances = [Double]()
+        //looping to get all the location distance from user
+        for location in self.locations {
+            let distance = self.locationManager.location?.distance(from: CLLocation(latitude: CLLocationDegrees(location.locationLat!), longitude: CLLocationDegrees(location.locationLng!)))
+            location.distanceFromUser = distance as Double!
+            distances.append(distance!)
+        }
+        self.locations.sort(by: {$0.distanceFromUser! < $1.distanceFromUser!})
+    }
+
 
 }
